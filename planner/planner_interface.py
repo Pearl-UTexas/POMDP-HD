154a3d0930a8e93ac73cf50fd1f0a33b6099b69d
Ajinkya Jain <jainajinkya92@gmail.com>
2018-10-19 18:21:28 -0500

    Added documentation to planner_interface. Updated plotting function in utils

--- planner/planner_interface.py
+++ planner/planner_interface.py
@@ -5,95 +5,146 @@ import copy
 from pomdp_hd_planner import *
 from planner_core.src.blqr import *
 
+
 class planner_interface:
     def __init__(self, x, mu, goal):
-        # Class initializations
+        ''' Object initializations '''
         self.planner = pomdp_hd()
-        self.controller = blqr(self.planner.nState, self.planner.nInput, self.planner.nOutput, self.planner.opt.Q, self.planner.opt.R, self.planner.opt.labda, self.planner.opt.Q_f)
+        self.controller = blqr(self.planner.nState, self.planner.nInput,
+                               self.planner.nOutput, self.planner.opt.Q,
+                               self.planner.opt.R, self.planner.opt.labda,
+                               self.planner.opt.Q_f)
 
-        # Set Parmeters
+        ''' Set Planner Parmeters'''
         self.planner.do_parallelize = True
         self.planner.do_verbose = False
-
         # Number fo Samples to calculate discerte belief from continous beleif
-        self.planner.dyna.nSamples = 50. 
-        
+        self.planner.dyna.nSamples = 50.
         # Change the loop count in beleif propagation int value
-        self.planner.dyna.ds_res_loop_count_ = 100 
+        self.planner.dyna.ds_res_loop_count_ = 100
 
-        # Set Values
+        ''' Initialization '''
         self.id1 = self.planner.opt.id1
         self.x_actual = copy.copy(x)
         self.mu_actual = copy.copy(mu)
-        self.cov_actual = (np.linalg.norm(x-mu)**2 + 25.)*np.eye(self.planner.nState)
+        self.cov_actual = (np.linalg.norm(x-mu)**2 + 25.) * \
+            np.eye(self.planner.nState)
         self.s_actual = copy.copy(self.cov_actual[self.id1])
         self.wts_actual = np.ndarray(self.planner.nModel)
         self.goal = copy.copy(goal)
 
         self.planner.start_mu = copy.copy(self.mu_actual)
         self.planner.start_cov = copy.copy(self.cov_actual)
-        self.planner.belief = hybrid_belief(self.mu_actual, self.cov_actual, self.wts_actual)
+        self.planner.belief = hybrid_belief(
+            self.mu_actual, self.cov_actual, self.wts_actual)
         self.planner.goal_cs = copy.copy(self.goal)
-        self.planner.dyna.setGC(copy.copy(self.goal)) # Setting GC
+        self.planner.dyna.setGC(copy.copy(self.goal))  # Setting GC
 
         # Matrices for holding dynamics
         self.A = np.ndarray((self.planner.nState, self.planner.nState))
         self.B = np.ndarray((self.planner.nInput, self.planner.nInput))
         self.C = np.ndarray((self.planner.nOutput, self.planner.nOutput))
-        self.V =  np.ndarray((self.planner.nState, self.planner.nState))
+        self.V = np.ndarray((self.planner.nState, self.planner.nState))
         self.W = np.ndarray((self.planner.nOutput, self.planner.nOutput))
         self.idx = 0
         self.idx_old = 0
-        self.cov_plan = symarray(np.zeros((self.planner.nState, self.planner.nState)))
+        self.cov_plan = symarray(
+            np.zeros((self.planner.nState, self.planner.nState)))
         self.xNew = np.ndarray(self.planner.nState)
         self.z = np.ndarray(self.planner.nState)
         self.muNew = np.ndarray(self.planner.nState)
         self.covNew = np.ndarray((self.planner.nState, self.planner.nState))
         self.wtsNew = np.ndarray(self.planner.nModel)
 
-        # Data Storage
+        ''' Data Storage '''
         self.traj = [copy.copy(self.mu_actual)]
         self.traj_true = [copy.copy(self.x_actual)]
         self.ds_traj = [copy.copy(self.wts_actual)]
         self.cov_traj = [copy.copy(self.s_actual)]
 
-        #### Checking if everything is correctly set
-        print "start_mu: ", self.planner.start_mu, "\nstart_cov:", self.planner.start_cov, "\ngoal: ", self.goal
-        # raw_input('Press Enter if everything looks okay!')
-
+        # Checking if everything is correctly set
+        print "start_mu: ", self.planner.start_mu
+        print "start_cov:", self.planner.start_cov, "\ngoal: ", self.goal
 
     def generate_plan(self):
+        """
+        Function to generate plan based on set continuous space target
+
+        Returns
+        -------
+            mu_plan : planned belief mean trajectory
+            s_plan  : Covariance vector based on planned trajectory
+            u_plan  : Planned control to be applied 
+        """
         start_time = time.time()
         mu_plan, s_plan, u_plan = self.planner.plan_optimal_path()
         self.planner.first_pass = False
 
         tActual = np.round((time.time() - start_time), 3)
-        print("Total time required by Planner = %s seconds "  %tActual)
+        print("Total time required by Planner = %s seconds " % tActual)
         return mu_plan, s_plan, u_plan
 
     def execute_plan_oneStep(self, mu_plan, s_plan, u_plan):
+        """
+        Function to execute plan one step based on applied controls.
+        First it calculates the local belief space control (B-LQR) 
+        that it should apply based on the current belief and covariance.
+        Executes one step using simulator and returns new robot state 
+        and observation
+
+        Parameters
+        -------
+            mu_plan : Planned next step belief mean
+            s_plan  : Planned next step belief covariance
+            u_plan  : Planned control input
+
+    
+        Returns
+        -------
+            xNew    : New robot state from simulator
+            z       : Observation from simulator
+
+        """
         self.cov_plan[self.id1] = copy.copy(s_plan)
 
-        #LQR Control
-        self.planner.dyna.getMatrices(self.idx, self.A, self.B, self.C, self.V, self.W)
+        # B-LQR Control
+        self.planner.dyna.getMatrices(
+            self.idx, self.A, self.B, self.C, self.V, self.W)
 
-        u_local = self.controller.blqr(self.mu_actual, self.s_actual, mu_plan, s_plan, u_plan, self.A, self.B, self.C, self.W, 5)
+        u_local = self.controller.blqr(
+            self.mu_actual, self.s_actual, mu_plan,
+            s_plan, u_plan, self.A, self.B, self.C, self.W, 5)
 
         # Propagation of the Belief continuous Dynamics
-        self.idx = self.planner.dyna.predictionStochastic(self.mu_actual, self.cov_actual, u_local, self.muNew, self.covNew, self.wtsNew, self.idx)
+        self.idx = self.planner.dyna.predictionStochastic(
+            self.mu_actual, self.cov_actual, u_local,
+            self.muNew, self.covNew, self.wtsNew, self.idx)
 
         # Next point on the Trajectory
-        z_dummy = np.ndarray(self.planner.nState)
-        self.planner.dyna.simulate_oneStep(self.x_actual, u_local, self.xNew, z_dummy)
+        z = np.ndarray(self.planner.nState)
+        self.planner.dyna.simulate_oneStep(
+            self.x_actual, u_local, self.xNew, z)
 
-        return self.xNew, z_dummy
+        return self.xNew, z
 
     def update_belief(self, z):
-        #Updates based on the observation
-        self.idx = self.planner.dyna.observationUpdate(z, self.muNew, self.covNew, self.wtsNew, self.idx)
+        """
+        Update belief based on received observation
+
+        Parameters
+        -------
+            z       : Recievd Observation
+    
+        """
+        # Updates based on the observation
+        self.idx = self.planner.dyna.observationUpdate(
+            z, self.muNew, self.covNew, self.wtsNew, self.idx)
         return
 
     def update_stored_values(self):
+        """
+        Updates values in the planner
+        """
         # Update belief Values
         self.mu_actual = copy.copy(self.muNew.T)
         self.planner.start_mu = copy.copy(self.mu_actual)
@@ -103,7 +154,17 @@ class planner_interface:
         self.s_actual = copy.copy(self.cov_actual[self.id1])
         self.wts_actual = copy.copy(self.wtsNew)
 
-    def trigger_restart(self, mu=np.random.rand(3) ,s=2.0):
+    def trigger_restart(self, mu=np.random.rand(3), s=2.0):
+        """
+        Trigger Random restart of belief in case of too much divergence.
+        Updates internal values
+
+        Parameters
+        -------
+            mu      : Belief mean to start with
+            s       : Belief covariance to start with
+
+        """
         self.mu = mu
         # self.s = s
         self.s[0] = s
@@ -113,32 +174,27 @@ class planner_interface:
         self.s[4] = 0.
         self.s[5] = s
         self.cov[self.id1] = self.s
-        print "##############\nTriggering belief restart with \n", "Mean =", self.mu, "\nCovariance =", self.cov, "\n###############"
+        print "############################"
+        print "Triggering belief restart with "
+        print "Mean =", self.mu, "\nCovariance =", self.cov
+        print "############################"
         return
 
     def observation_triggered_restart(self, observation_array):
-        # Force restart if the covariance on continuoues states is very low but observations suggest mean is too diverged
-        obs_mean = np.mean(observation_array,axis=0)
+        """
+        Force restart if the covariance on continuoues states
+        is very low but observations suggest mean is too diverged
+
+        Parameters
+        -------
+            observation_array  : An array of observations from last 
+                                 'n' steps
+
+        """
+        obs_mean = np.mean(observation_array, axis=0)
 
         if max(self.s) < 0.01:
             if distance.euclidean(obs_mean, self.mu) > 0.01:
                 self.trigger_restart(mu=obs_mean)
 
         return
-
-
-    def plotter():
-        import matplotlib.pyplot as plt
-        fig = plt.figure
-        x_vec = [traj[i][0] for i in range(len(traj))]
-        y_vec = [traj[i][1] for i in range(len(traj))]
-        x_vec_true = [traj_true[i][0] for i in range(len(traj_true))]
-        y_vec_true = [traj_true[i][1] for i in range(len(traj_true))]
-        plt.plot(x_vec,y_vec, 'r--')
-        plt.plot(x_vec_true,y_vec_true, 'b',linewidth = 3.0)
-
-        axes = plt.gca()
-        # axes.set_xlim([xmin,xmax])
-        axes.set_ylim([-2.0, 3.0])
-        plt.show()
-    
--- planner/utils/plotting.py
+++ planner/utils/plotting.py
@@ -1,6 +1,8 @@
 import numpy as np
+import sys
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
+
 
 def plot_point_cov(points, nstd=2, ax=None, **kwargs):
     """
@@ -23,6 +25,7 @@ def plot_point_cov(points, nstd=2, ax=None, **kwargs):
     pos = points.mean(axis=0)
     cov = np.cov(points, rowvar=False)
     return plot_cov_ellipse(pos, cov, nstd, ax, **kwargs)
+
 
 def plot_cov_ellipse(pos, cov, nstd=1, ax=None, **kwargs):
     """
@@ -48,13 +51,13 @@ def plot_cov_ellipse(pos, cov, nstd=1, ax=None, **kwargs):
     def eigsorted(cov):
         vals, vecs = np.linalg.eigh(cov)
         order = vals.argsort()[::-1]
-        return vals[order], vecs[:,order]
+        return vals[order], vecs[:, order]
 
     if ax is None:
         ax = plt.gca()
 
     vals, vecs = eigsorted(cov)
-    theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+    theta = np.degrees(np.arctan2(*vecs[:, 0][::-1]))
 
     # Width and height are "full" widths, not radius
     width, height = 2 * nstd * np.sqrt(vals)
@@ -63,18 +66,130 @@ def plot_cov_ellipse(pos, cov, nstd=1, ax=None, **kwargs):
     ax.add_artist(ellip)
     return ellip
 
+
+def plot_trajectories(traj, traj_true, cov_traj, idx=0, save_fig=False):
+    """
+    Plot trajectory 
+
+    Parameters
+    ----------
+        traj : Belief Trajectory
+        traj_true  : Actual Trajectory from Simulation
+        cov_traj: Covariance Values recorded along the trajectory
+        idx: Plot images till this time step
+        save_fig: Whether or not should save figure   
+
+    """
+
+    print "#############################################"
+    print "Final Trajecotry"
+    for i in range(len(traj)):
+        print "Step: ", i
+        print "Planned Trajectory:", traj[i]
+        print "True Trajectory   :", traj_true[i]
+    print "#############################################\n"
+
+    fig = plt.figure(figsize=(15, 15))
+
+    ## Setting Up Domain ---  DEPENDS ON EACH DOMAIN. PLEASE SET IT UP ACCORDINGLY
+    x1 = -2.10
+    y1 = -2.10
+    x2 = 10.0
+    y2 = -2.10
+    plt.plot([x1, x2], [y1, y2], color='k', linestyle='-', linewidth=30)
+
+    x1 = -2.10
+    y1 = -2.10
+    x2 = -2.10
+    y2 = 10.0
+    plt.plot([x1, x2], [y1, y2], color='k', linestyle='-', linewidth=30)
+
+
+    ## Plot belief Trajectory
+    x_vec = [traj[i][0] for i in range(len(traj))]
+    y_vec = [traj[i][1] for i in range(len(traj))]
+
+    ## Plot actual Trajectory
+    x_vec_true = [traj_true[i][0] for i in range(len(traj_true))]
+    y_vec_true = [traj_true[i][1]
+                  for i in range(len(traj_true))]
+
+    bl1, = plt.plot(x_vec, y_vec, 'r-o', linewidth=5.0, ms=15.0)
+    bl2, = plt.plot(x_vec_true, y_vec_true, 'b-D', linewidth=5.0, ms=15.0)
+
+    ## Plot covariance
+    for i in np.arange(0, len(x_vec), 2):
+        covar = np.array([[cov_traj[i][0], cov_traj[i][1]],
+                          [cov_traj[i][1], cov_traj[i][2]]])
+        plot_cov_ellipse(traj[i], covar, nstd=1., alpha=0.25, color='k')
+
+    ## Plot Initial Point
+    plt.plot(x_vec[0], y_vec[0], 'ko', ms=15.0, mew=5.0)
+    plt.plot(x_vec_true[0], y_vec_true[0], 'ko', ms=15.0, mew=5.0)
+
+    ## Plot Goal Point
+    plt.plot(0., 0., 'gs', ms=18.0, mew=5.0)
+
+    ## Set PLot Parameters
+    plt.xlim([-2.00, 10.0])
+    plt.ylim([-2.00, 10.00])
+    plt.xlabel('x axis (in cm)', fontsize=25)
+    plt.ylabel('y axis (in cm)', fontsize=25)
+    plt.title('Trajectories for Discrete state based planning', fontsize=30)
+    plt.legend([bl1, bl2], ['Belief Trajectory',
+                            'Actual Trajectory'], fontsize=25)
+
+    plt.tick_params(labelsize=20)
+
+    if save_fig:
+        plt.savefig('../data/trajPlots/Step ' + str(idx), bbox_inches='tight')
+    else:
+        fig.show()
+        raw_input('Press Enter to close Plot')
+    plt.close()
+
+
+def plot_frame_by_frame(traj_mu, traj_x, cov_traj, save_fig=False):
+    """
+    Plot trajectory frame-by-frame (Useful for making animations)
+
+    Parameters
+    ----------
+        traj_mu : Belief Trajectory
+        traj_x  : Actual Trajectory from Simulation
+        cov_traj: Covariance Values recorded along the trajectory
+        save_fig: Whether or not should save figure   
+
+    """
+    for j in range(len(traj_mu)+1):
+        plot_trajectories(traj_mu[:j+1], traj_x[:j+1], cov_traj[:j+1], idx=j+1, save_fig=save_fig)
+
+
+def plot_full_traj(traj_mu, traj_x, cov_traj, save_fig=False):
+    """
+    Plot full trajectory (Single Plot)
+
+    Parameters
+    ----------
+        traj_mu : Belief Trajectory
+        traj_x  : Actual Trajectory from Simulation
+        cov_traj: Covariance Values recorded along the trajectory     
+        save_fig: Whether or not should save figure
+
+    """
+    plot_trajectories(traj_mu, traj_x, cov_traj, save_fig=save_fig)
+
+
 if __name__ == '__main__':
-    #-- Example usage -----------------------
-    # Generate some random, correlated data
-    for i in range(10):
-        points = np.random.multivariate_normal(
-                mean=(i,i), cov=[[0.4, 0],[0, 10]], size=100
-                )
-        # Plot the raw points...
-        x, y = points.T
-        plt.plot(x, y, 'ro')
+    if len(sys.argv) > 1:
+        f_name = sys.argv[1]
+    else:
+        f_name = "../data/better_res_2_labda1e7_q_f_1e4_r_10.npz"
 
-        # Plot a transparent 3 standard deviation covariance ellipse
-        plot_point_cov(points, nstd=1, alpha=1.0, color='k')
+    data = np.load(f_name)
+    traj = data['belief_traj']
+    traj_true = data['actual_traj']
+    cov_traj = data['cov_traj']
 
-    plt.show()
+    plot_full_traj(traj, traj_true, cov_traj)
+    # plot_frame_by_frame(traj, traj_true, cov_traj)
